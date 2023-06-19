141d5b60585670d0532307bb00ea8ebf050538dc
Dylan Wang <d428wang@uwaterloo.ca>
2023-06-18 21:40:06 -0400

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    './base/templates/base/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
