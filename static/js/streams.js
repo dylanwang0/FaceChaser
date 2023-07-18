20753f4443ad1ad9ee4e5809203f387ab412478d
Dylan Wang <d428wang@uwaterloo.ca>
2023-07-18 00:23:16 -0400

    started closed caption

--- static/js/streams.js
+++ static/js/streams.js
@@ -1,139 +1,181 @@
-const APP_ID = '7684fcafa4e447d18c2d0200b17ae684'
-const CHANNEL = sessionStorage.getItem('room')
-const TOKEN = sessionStorage.getItem('token')
-let UID = Number(sessionStorage.getItem('UID'))
+const APP_ID = "7684fcafa4e447d18c2d0200b17ae684";
+const CHANNEL = sessionStorage.getItem("room");
+const TOKEN = sessionStorage.getItem("token");
+let UID = Number(sessionStorage.getItem("UID"));
 
-let NAME = sessionStorage.getItem('name')
+let NAME = sessionStorage.getItem("name");
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+var transContent = "";
+var noteContent = "";
+recognition.continuous = true;
+var isLoggedIn = false;
+var transcriptActive = false;
 
-const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
+const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+//const clientRTM = AgoraRTM.createInstance();
 
-let localTracks = []
-let remoteUsers = {}
+let localTracks = [];
+let remoteUsers = {};
 
 let joinAndDisplayLocalStream = async () => {
-    document.getElementById('room-name').innerText = CHANNEL
+  document.getElementById("room-name").innerText = CHANNEL;
 
-    client.on('user-published', handleUserJoined)
-    client.on('user-left', handleUserLeft)
+  client.on("user-published", handleUserJoined);
+  client.on("user-left", handleUserLeft);
 
-    try{
-        await client.join(APP_ID, CHANNEL, TOKEN, UID)
-    }catch(error){
-        console.error(error)
-        window.open('/', '_self')
-    }
+  try {
+    await client.join(APP_ID, CHANNEL, TOKEN, UID);
+  } catch (error) {
+    console.error(error);
+    window.open("/", "_self");
+  }
 
+  localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
 
-    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+  let member = await createMember();
 
-    let member = await createMember()
-
-    let player = `<div class="video-container" id="user-container-${UID}">
+  let player = `<div class="video-container" id="user-container-${UID}">
                     <div class="username-wrapper"><span class="user-name">${member.name}</span></div>
                     <div class="video-player" id="user-${UID}"></div>
-                </div>`
-    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+                </div>`;
+  document
+    .getElementById("video-streams")
+    .insertAdjacentHTML("beforeend", player);
 
-    localTracks[1].play(`user-${UID}`)
+  localTracks[1].play(`user-${UID}`);
 
-    await client.publish([localTracks[0], localTracks[1]])
-}
+  await client.publish([localTracks[0], localTracks[1]]);
+};
 
 let handleUserJoined = async (user, mediaType) => {
-    remoteUsers[user.uid] = user
-    await client.subscribe(user, mediaType)
+  remoteUsers[user.uid] = user;
+  await client.subscribe(user, mediaType);
 
-    if (mediaType === 'video'){
-        let player = document.getElementById(`user-container-${user.uid}`)
-        if (player != null){
-            player.remove()
-        }
+  if (mediaType === "video") {
+    let player = document.getElementById(`user-container-${user.uid}`);
+    if (player != null) {
+      player.remove();
+    }
 
-        let member = await getMember(user)
+    let member = await getMember(user);
 
-        player = `<div class="video-container" id="user-container-${user.uid}">
+    player = `<div class="video-container" id="user-container-${user.uid}">
                     <div class="username-wrapper"><span class="user-name">${member.name}</span></div>
                     <div class="video-player" id="user-${user.uid}"></div>
-                </div>`
-        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+                </div>`;
+    document
+      .getElementById("video-streams")
+      .insertAdjacentHTML("beforeend", player);
 
-        user.videoTrack.play(`user-${user.uid}`)
-    }
+    user.videoTrack.play(`user-${user.uid}`);
+  }
 
-    if (mediaType === 'audio'){
-        user.audioTrack.play()
-    }
-}
+  if (mediaType === "audio") {
+    user.audioTrack.play();
+  }
+};
 
 let handleUserLeft = async (user) => {
-    delete remoteUsers[user.uid]
-    document.getElementById(`user-container-${user.uid}`).remove()
-}
+  delete remoteUsers[user.uid];
+  document.getElementById(`user-container-${user.uid}`).remove();
+};
 
 let leaveAndRemoveLocalStream = async () => {
-    for (let i = 0; i < localTracks.length; i++){
-        localTracks[i].stop()
-        localTracks[i].close()
-    }
+  for (let i = 0; i < localTracks.length; i++) {
+    localTracks[i].stop();
+    localTracks[i].close();
+  }
 
-    await client.leave()
-    deleteMember()
-    window.open('/', '_self')
-}
+  await client.leave();
+  deleteMember();
+  window.open("/", "_self");
+};
 
 let toggleCamera = async (e) => {
-    if (localTracks[1].muted){
-        await localTracks[1].setMuted(false)
-        e.target.style.backgroundColor = '#fff'
-    } else{
-        await localTracks[1].setMuted(true)
-        e.target.style.backgroundColor = 'rgb(255, 80, 80, 1)'
-    }
-}
+  if (localTracks[1].muted) {
+    await localTracks[1].setMuted(false);
+    e.target.style.backgroundColor = "#fff";
+  } else {
+    await localTracks[1].setMuted(true);
+    e.target.style.backgroundColor = "rgb(255, 80, 80, 1)";
+  }
+};
 
 let toggleMic = async (e) => {
-    if (localTracks[0].muted){
-        await localTracks[0].setMuted(false)
-        e.target.style.backgroundColor = '#fff'
-    } else{
-        await localTracks[0].setMuted(true)
-        e.target.style.backgroundColor = 'rgb(255, 80, 80, 1)'
-    }
-}
+  if (localTracks[0].muted) {
+    await localTracks[0].setMuted(false);
+    e.target.style.backgroundColor = "#fff";
+  } else {
+    await localTracks[0].setMuted(true);
+    e.target.style.backgroundColor = "rgb(255, 80, 80, 1)";
+  }
+};
+
+let toggleTranscript = async (e) => {
+  if (transcriptActive) {
+    recognition.stop();
+    e.target.style.backgroundColor = "#fff";
+    transcriptActive = false
+  } else {
+    recognition.start();
+    e.target.style.backgroundColor = "rgb(255, 80, 80, 1)";
+    transcriptActive = true
+  }
+};
+
+transcriptText = document.getElementById("transcriptText")
+
+recognition.onresult = (event) => {
+    var current = event.resultIndex;
+  const text = event.results[current][0].transcript;
+  const confidence = event.results[current][0].confidence;
+  if (!localTracks[0].muted){
+    transcriptText.innerText = NAME + ": " + text;
+  }
+  
+};
 
 let createMember = async () => {
-    let response = await fetch('/create_member/', {
-        method: 'POST',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify({'name':NAME, 'room_name':CHANNEL, 'UID':UID})
-    })
-    let member = await response.json()
-    return member
-}
+  let response = await fetch("/create_member/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name: NAME, room_name: CHANNEL, UID: UID }),
+  });
+  let member = await response.json();
+  return member;
+};
 
 let getMember = async (user) => {
-    let response = await fetch(`/get_member/?UID=${user.uid}&room_name=${CHANNEL}`)
-    let member = await response.json()
-    return member
-}
+  let response = await fetch(
+    `/get_member/?UID=${user.uid}&room_name=${CHANNEL}`
+  );
+  let member = await response.json();
+  return member;
+};
 
 let deleteMember = async () => {
-    let response = await fetch('/delete_member/', {
-        method: 'POST',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify({'name':NAME, 'room_name':CHANNEL, 'UID':UID})
-    })
-    let member = await response.json()
-}
+  let response = await fetch("/delete_member/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name: NAME, room_name: CHANNEL, UID: UID }),
+  });
+  let member = await response.json();
+};
 
-joinAndDisplayLocalStream()
+joinAndDisplayLocalStream();
 
-window.addEventListener('beforeunload', deleteMember)
+window.addEventListener("beforeunload", deleteMember);
 
-document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveLocalStream)
-document.getElementById('camera-btn').addEventListener('click', toggleCamera)
-document.getElementById('mic-btn').addEventListener('click', toggleMic)
+document
+  .getElementById("leave-btn")
+  .addEventListener("click", leaveAndRemoveLocalStream);
+document.getElementById("camera-btn").addEventListener("click", toggleCamera);
+document.getElementById("mic-btn").addEventListener("click", toggleMic);
+document.getElementById("trans-btn").addEventListener("click", toggleTranscript);
+
